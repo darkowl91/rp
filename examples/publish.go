a744271e010bd52e95b18db8ea78eb23f06d3974
Nickolay <darkowl91@gmail.com>
2017-05-05 00:26:04 +0300

    xml report

--- examples/publish.go
+++ examples/publish.go
@@ -5,10 +5,10 @@ import (
 )
 
 const (
-	uuid      = ""
-	project   = ""
+	uuid      = "26169e7b-f3fc-46a8-8ef6-a41f8ee30fcd"
+	project   = "WFR-API"
 	reportDir = "report"
-	apiURL    = ""
+	apiURL    = "http://10.48.128.12:80/api/v1/"
 )
 
 func main() {
@@ -22,8 +22,8 @@ func main() {
 	}
 
 	// start, end for launch
-	launchStart := report.GetLaunchStartTime()
-	launchEnd := report.GetLaunchEndTime()
+	launchStart := report.LaunchStartTime()
+	launchEnd := report.LaunchEndTime()
 
 	// create new Report Portal client
 	rpClient := rp.NewClient(apiURL, project, uuid)
@@ -36,12 +36,37 @@ func main() {
 		Tags:      []string{"R50", "debug", "go"},
 	})
 
-	// TODO:
+	// start post report
+	for i := 0; i < report.SuitesCount(); i++ {
+		// create suite
+		suite := report.Suite(i)
+		suite.LaunchID = launchID.ID
+		suiteID := rpClient.StartTestItem("", suite)
+
+		// start post cases
+		for j := 0; j < report.TesCaseCount(i); j++ {
+			//create test case
+			tCase := report.TestCase(i, j)
+			tCase.LaunchID = launchID.ID
+			caseID := rpClient.StartTestItem(suiteID.ID, tCase)
+
+			// pos log in case we have failure
+			if report.HasTestCasefailure(i, j) {
+				// post message
+				failure := report.TestCasefailure(i, j)
+				failure.ItemID = caseID.ID
+				rpClient.SendMesssage(failure)
+			}
+			tResult := report.TestCaseResult(i, j)
+			rpClient.FinishTestItem(caseID.ID, tResult)
+		}
+		suiteResult := report.SuiteResult(i)
+		rpClient.FinishTestItem(suiteID.ID, suiteResult)
+	}
 
 	// update launch to completed state
 	rpClient.FinishLaunch(launchID.ID, &rp.ExecutionResult{
 		EndTime: launchEnd,
-		Status:  rp.ExecutionStatusPassed, // launch always passed
 	})
 
 }
--- report.go
+++ report.go
@@ -3,6 +3,7 @@ package rp
 import (
 	"encoding/xml"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -60,16 +61,107 @@ func LoadXMLReport(dirName string) (*XMLReport, error) {
 }
 
 //
-func (report *XMLReport) GetLaunchStartTime() time.Time {
-	return parseTimeStamp(report.xmlSuites[0].TimeStamp)
+func (report *XMLReport) SuitesCount() int {
+	return len(report.xmlSuites)
 }
 
 //
-func (report *XMLReport) GetLaunchEndTime() time.Time {
+func (report *XMLReport) TesCaseCount(i int) int {
+	return len(report.xmlSuites[i].Cases)
+}
+
+// LaunchStartTime is used to calc launch time, it will be equal to 0 suite start time
+func (report *XMLReport) LaunchStartTime() time.Time {
+	return parseTimeStamp(report.xmlSuites[0].TimeStamp)
+}
+
+// LaunchEndTime is used to calc launch end time, it will be equal to last suite start time plus last suite duration
+func (report *XMLReport) LaunchEndTime() time.Time {
 	lastIndex := len(report.xmlSuites) - 1
 	lastSuiteStart := parseTimeStamp(report.xmlSuites[lastIndex].TimeStamp)
 	duration := time.Duration(report.xmlSuites[lastIndex].Time)
 	return lastSuiteStart.Add(duration)
+}
+
+//
+func (report *XMLReport) Suite(i int) *TestItem {
+	xSuite := report.xmlSuites[i]
+	suiteStart := parseTimeStamp(xSuite.TimeStamp)
+	return &TestItem{
+		Type:        TestItemTypeSuite,
+		StartTime:   suiteStart,
+		Name:        xSuite.PackageName + "." + xSuite.Name,
+		Description: fmt.Sprintf("%s %d", TestItemTypeSuite, xSuite.ID),
+	}
+}
+
+//
+func (report *XMLReport) SuiteResult(i int) *ExecutionResult {
+	xSuite := report.xmlSuites[i]
+	suiteStart := parseTimeStamp(xSuite.TimeStamp)
+	suiteEnd := suiteStart.Add(time.Duration(xSuite.Time))
+
+	var status = ExecutionStatusPassed
+	if xSuite.Tests == 0 {
+		status = ExecutionStatusSkipped
+	} else if xSuite.Failures > 0 {
+		status = ExecutionStatusFailed
+	} else if xSuite.Errors > 0 {
+		status = ExecutionStatusFailed
+	}
+
+	return &ExecutionResult{
+		EndTime: suiteEnd,
+		Status:  status,
+	}
+}
+
+//
+func (report *XMLReport) TestCase(i, j int) *TestItem {
+	xSuite := report.xmlSuites[i]
+	suiteStart := parseTimeStamp(xSuite.TimeStamp)
+	xCase := xSuite.Cases[j]
+	return &TestItem{
+		Type:      TestItemTypeStep,
+		Name:      xCase.Name,
+		StartTime: suiteStart, // FixMe
+	}
+}
+
+//
+func (report *XMLReport) TestCaseResult(i, j int) *ExecutionResult {
+	xSuite := report.xmlSuites[i]
+	suiteStart := parseTimeStamp(xSuite.TimeStamp)
+	xCase := xSuite.Cases[j]
+	xCaseEnd := suiteStart.Add(time.Duration(xCase.Time))
+	var status = ExecutionStatusPassed
+	if xCase.Failure != nil {
+		status = ExecutionStatusFailed
+	}
+
+	return &ExecutionResult{
+		EndTime: xCaseEnd,
+		Status:  status,
+	}
+}
+
+//
+func (report *XMLReport) HasTestCasefailure(i, j int) bool {
+	return report.xmlSuites[i].Cases[j].Failure != nil
+}
+
+//
+func (report *XMLReport) TestCasefailure(i, j int) *LogMessage {
+	xSuite := report.xmlSuites[i]
+	suiteStart := parseTimeStamp(xSuite.TimeStamp)
+	xCase := xSuite.Cases[j]
+	xCaseEnd := suiteStart.Add(time.Duration(xCase.Time))
+
+	return &LogMessage{
+		Time:    xCaseEnd,
+		Level:   LogLevelError,
+		Message: xCase.Failure.Message,
+	}
 }
 
 // parseXMLReport is used for parsing xml report sorted by suite start time
