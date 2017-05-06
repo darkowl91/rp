77db8b3874623424192c19644ba4b71e8429f810
Nickolay <darkowl91@gmail.com>
2017-05-06 12:23:45 +0300

    usage for cmd toool

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const (
+	version = "0.1a"
+)
+
+var (
+	reportDir string
+)
+
 func main() {
+	flag.Usage = func() {
+		u := " Usage:\n"
+		u += "  rp [OPTIONS] (DIR|FILE)\n\n"
+		u += "Options:\n"
+		u += "	-r,	--report	XML Report dir path\n"
+		u += "	-H	--rpHost	Report Portal host\n"
+		u += "	-m	--mode		Report Portal mode\n"
+		u += "	-p	--project	Report Portal project\n"
+		u += "	-l	--launch	Report Portal launch name\n"
+		u += "	-id	--uuid		Report Portal user id\n"
+		u += "	-h,	--help		Print usage\n"
+		u += "	-v,	--version	Print version information and quit\n\n"
+		fmt.Fprintf(os.Stdout, u)
+	}
+
+	flag.Usage()
 
 }
